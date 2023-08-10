c58206417df9f95e800989fd06a06cbb74c29f96
plxulo <pcfdaa@gmail.com>
2023-08-09 21:46:31 -0300

    fixes in footer, fixed id names and some style issues, gonna fix and polish everything tomorrow

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -23,9 +23,9 @@
     </header>
 
     <!-- NAVEGAÇÃO: -->
-    <section aria-labelledby="navegar_title" class="group_1 flex flex-col items-center gap-2.5">
-      <h1 id="navegar_title" class="font-poppins font-bold leading-10 text-[2.5rem] text-preto"></h1>
-      <ul class="flex flex-col text-cinza font-poppins font-normal leading-5">
+    <section aria-labelledby="navegar_title" class="group_1 flex flex-col items-center gap-2.5 self-stretch">
+      <h1 id="navegar_title" class="font-poppins font-bold leading-10 text-[2.5rem] text-preto">Navegar</h1>
+      <ul class="flex flex-col text-cinza font-poppins font-normal leading-5 w-full">
         <li>Principal</li>
         <li>Login</li>
         <li>Ocorrências</li>
@@ -34,10 +34,10 @@
     </section>
 
     <!-- CONTATO: -->
-    <section class="contato flex flex-col items-center gap-2.5 self-stretch">
+    <section aria-labelledby="contato_title" class="contato flex flex-col items-center gap-2.5 self-stretch">
 
-      <h1 id="navegar_title" class="font-poppins font-bold leading-10 text-[2.5rem] text-preto">Contato</h1>
-      <ul class="flex flex-col text-cinza font-poppins font-normal leading-5">
+      <h1 id="contato_title" class="font-poppins font-bold leading-10 text-[2.5rem] text-preto">Contato</h1>
+      <ul class="flex flex-col text-cinza font-poppins font-normal leading-5 w-full">
         <div class="text flex flex-row gap-2.5">
           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="20" viewBox="0 0 14 20" fill="none">
             <path d="M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z" fill="#555555"/>
