9b543b4c373294cbaa4df22c3221e5e6812fa8ed
PAULO CESAR FERREIRA DO AMARANTE <paulo_amarante@estudante.sesisenai.org.br>
2023-09-20 12:23:15 -0300

    cock and balls

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,14 +1,12 @@
-<!-- Importar este arquivo include para toda página que desejar exibir o footer com PHP -->
 <footer class="flex flex-col justify-center items-start gap-[0.625rem] w-full h-full self-stretch bg-white
-  desktop:flex-col desktop:items-start desktop:px-16
+  desktop:flex-col desktop:items-start desktop:border-t-[4px] desktop:border-vermelho
 ">
-
   <section class="container_footer flex flex-col justify-center items-center gap-5 p-8 self-stretch
     desktop:justify-between desktop:items-center desktop:flex-row
   ">
     <section class="logos_row flex flex-row justify-center items-center gap-4 self-stretch">
-      <img src="../public/images/logo-noar.png" alt="Logo Noar">
-      <img src="../public/images/ambulancia.png" alt="Logo Ambulância">
+      <img src="../public/images/logo-noar.png" alt="Logo Noar" class="w-[126px] h-[126px] desktop:w-[192px] desktop:h-[192px]">
+      <img src="../public/images/ambulancia.png" alt="Logo Ambulância" class="w-[126px] h-[126px] desktop:w-[192px] desktop:h-[192px]">
       <header aria-labelledby="footer_title" class="header hidden desktop:flex flex-col justify-center items-start gap-2.5 self-stretch">
         <h1 id="footer_title" class="text-4xl font-poppins font-bold leading-9 text-preto">SOS Bombeiros</h1>
         <p class="self-stretch text-cinza font-poppins text-xl font-normal leading-5 w-96">
@@ -16,45 +14,39 @@
         </p>
       </header>
     </section>
-
     <header aria-labelledby="footer_title_mobile" class="header flex desktop:hidden flex-col justify-center items-start gap-2.5 self-stretch">
       <h1 id="footer_title_mobile" class="text-4xl font-poppins font-bold leading-9 text-preto">SOS Bombeiros</h1>
-      <p class="self-stretch text-cinza font-poppins text-xl font-normal leading-5 w-fill">
+      <p class="text-cinza font-poppins text-xl font-normal leading-5 w-fit">
         Fazendo uso da <b>tecnologia</b> para <b>otimizar</b> o atendimento de ocorrências e facilitar a <b>comunicação</b> entre bombeiros e médicos
       </p>
     </header>
-
     <section aria-labelledby="navegar_title" class="group_1 flex flex-col items-start gap-2.5 self-stretch text-left">
       <h1 id="navegar_title" class="font-poppins font-bold leading-10 text-4xl text-preto">Navegar</h1>
       <ul class="flex flex-col text-cinza font-poppins font-normal text-xl leading-5 w-full">
-        <li>Principal</li>
-        <li>Login</li>
-        <li>Ocorrências</li>
-        <li>Dados</li>
+        <li class="hover:text-black"><a href="">Principal</a></li>
+        <li class="hover:text-black"><a href="">Login</a></li>
+        <li class="hover:text-black"><a href="">Ocorrências</a></li>
+        <li class="hover:text-black"><a href="">Dados</a></li>
       </ul>
     </section>
-
     <section aria-labelledby="contato_title" class="contato flex flex-col items-start gap-2.5 self-stretch text-left">
-      <!-- Arrumar SVGs -->
       <h1 id="contato_title" class="font-poppins font-bold leading-10 text-4xl text-preto text-left">Contato</h1>
       <ul class="flex flex-col text-cinza font-poppins font-normal text-xl leading-5 w-full">
         <div class="text flex flex-row gap-2.5">
-          <a href="">Av. Procópio Gomes, 244</a>
+          <a href="" class="hover:text-black">Av. Procópio Gomes, 244</a>
         </div>
         <div class="text flex flex-row gap-2.5">
-          <a href="">+55 (47) 99735-5556</a>
+          <a href="" class="hover:text-black">+55 (47) 99735-5556</a>
         </div>
         <div class="text flex flex-row gap-2.5">
-          <a href="">paulo@email.com</a>
+          <a href="" class="hover:text-black">paulo@email.com</a>
         </div>
         <div class="text flex flex-row gap-2.5">
-          <a href="">@plxulo</a>
+          <a href="" class="hover:text-black">@plxulo</a>
         </div>
       </ul>
-
     </section>
   </section>
-
   <section class="container_footer2 flex flex-col px-8 py-16 w-full justify-center items-center gap-6 bg-vermelho font-poppins
     desktop:flex-row desktop:justify-between desktop:p-16 desktop:self-stretch desktop:text-center
   ">
@@ -69,5 +61,4 @@
     <hr class="desktop:hidden" width="100%">
     <p class="font-poppins text-white w-full">Copyright, 2023</p>
   </section>
-
 </footer>
--- inc/form-cadastro.inc.php
+++ inc/form-cadastro.inc.php
@@ -1,29 +1,54 @@
 <!-- Formulário -->
-<section aria-labelledby="title_cadastro" class="flex flex-col gap-2.5 w-fit" title="Form Container">
-  <header class="flex flex-col items-center">
-    <h1 id="title_cadastro" class="font-bold text-preto text-xl font-poppins"><span class="font-bold text-vermelho">Cadastre-se</span> no app</h1>
-    <p class="text-sm text-cinza">Insira suas informações para cadastro</p>
+<section aria-labelledby="title_cadastro" class="flex flex-col gap-2.5 tablet:w-full" title="Form Container">
+  <header class="flex flex-col items-center tablet:items-start">
+    <h1 id="title_cadastro" class="font-bold text-preto text-xl font-poppins tablet:text-4xl">
+      <span class="font-bold text-vermelho">Cadastre-se</span> no app
+    </h1>
+    <p class="text-sm text-cinza text-center">Insira suas informações para cadastro</p>
   </header>
-
+  
   <form class="flex flex-col gap-2.5" action="" method="" id="form_cadastro">
-    <div class="input_box flex flex-col g-2.5" title="Input Box">
-      <label for="nome">Nome:</label>
-      <input type="text" class="border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
-      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho" placeholder="Ex: Henrique Osmar Adelino" id="nome">
+    <div id="error" class="error_message error bg-error_bg border-2 border-border_error hidden flex-row gap-2.5 px-3 p-2.5 rounded-[30px] items-center self-stretch" title="Alerta" aria-label="Alerta">
+      <img src="../public/images/alert-icon.svg" alt="Alerta">
+      <p class="text-sm text-vermelho font-poppins">Há campos inválidos</p>
     </div>
-    <div class="input_box flex flex-col g-2.5" title="Input Box">
-      <label for="nome">N° Fibra:</label>
-      <input type="text" class="border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
-      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho" placeholder="Ex: 4200" id="n_fibra">
+    
+    <div class="input_box flex flex-col gap-2.5" title="Input Box">
+      <label for="input_name">Nome:</label>
+      <input id="input_name" type="text" class="input border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
+      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho focus:bg-white" placeholder="Ex: Henrique Osmar Adelino" required>
     </div>
-    <div class="input_box flex flex-col g-2.5" title="Input Box">
-      <label for="nome">Senha:</label>
-      <input type="password" class="border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
-      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho" placeholder="***" id="senha">
+    <div class="input_box flex flex-col gap-2.5" title="Input Box">
+      <label for="num_fibra">N° Fibra:</label>
+      <input id="num_fibra" type="text" class="input border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
+      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho focus:bg-white" placeholder="Ex: 4200">
     </div>
-    <a href="#" onclick="event.preventDefault()" class="underline font-normal text-cinza">Ao se cadastrar você concorda com os <b>termos de uso</b></a>
-    <button type="submit" onclick="event.preventDefault()" class="px-6 py-4 gap-2.5 self-stretch flex items-center justify-center bg-vermelho font-poppins font-bold text-xl text-white
-      transition ease-in-out hover:scale-105">Cadastrar</button>
+    <div class="input_box flex flex-col gap-2.5" title="Input Box">
+      <label for="input_senha">Senha:</label>
+      <input id="input_senha" type="password" class="input border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
+      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho focus:bg-white" placeholder="******">
+    </div>
+    <div class="input_box flex flex-col gap-2.5" title="Input Box">
+      <label for="admin">Administrador:</label>
+      <div class="container_radio flex flex-row items-center gap-10">
+        <div class="flex items-center gap-2.5">
+          <input type="radio" id="adm_sim" name="escolha" value="sim" class="appearance-none w-5 h-5 border border-input_placeholder checked:bg-vermelho rounded-full"><label for="adm_sim">Sim</label>
+        </div>
+        <div class="flex items-center gap-2.5">
+          <input type="radio" id="adm_nao" name="escolha" value="nao" class="appearance-none w-5 h-5 border border-input_placeholder checked:bg-vermelho rounded-full"><label for="adm_nao">Não</label>
+        </div>
+      </div>
+    </div>
+    <div id="adm_container_mobile" class="input_box hidden flex-col g-2.5" title="Input Box">
+      <label for="adm_code_mobile">Registro de Comandante:</label>
+      <input id="adm_code_mobile" type="password" class="input border-b-2 border-[#595959] p-3 bg-input_color text-input_placeholder text-sm 
+      transition ease-in-out focus:scale-105 focus:text-black focus:outline-vermelho focus:bg-white" placeholder="Ex: 12665">
+    </div>
+    <button id="cadastrar" type="submit" onclick="event.preventDefault()" class="button px-6 py-4 gap-2.5 self-stretch flex items-center justify-center bg-vermelho font-poppins font-bold text-xl text-white
+      transition ease-in-out hover:scale-105 tablet:text-3xl">Cadastre-se</button>
     <a href="#" onclick="event.preventDefault()" class="underline text-sm font-normal text-cinza"><b>Clique aqui</b> caso já tenha cadastro</a>
   </form>
+  <div class="content hidden tablet:flex flex-row justify-center items-end self-stretch h-full">
+    <p class="text-xs text-cinza"><b>Todos</b> os direitos reservados, SenasTech, 2023</p>
+  </div>
 </section>
