072f4286baaa1a0a124071f14bd02c538e12e503
PAULO CESAR FERREIRA DO AMARANTE <paulo_amarante@estudante.sesisenai.org.br>
2023-09-26 12:28:49 -0300

    signup-controller

--- inc/class/signup-controller.class.php
+++ inc/class/signup-controller.class.php
@@ -8,11 +8,11 @@ class signupController {
   private $cmdt_code;
 
   public function __construct($username, $num_fibra, $pwd, $cmdt_radio, $cmdt_code) {
-    $this->username = $username;
-    $this->num_fibra = $num_fibra;
-    $this->pwd = $pwd;
+    $this->username   = $username;
+    $this->num_fibra  = $num_fibra;
+    $this->pwd        = $pwd;
     $this->cmdt_radio = $cmdt_radio;
-    $this->cmdt_code = $cmdt_code;
+    $this->cmdt_code  = $cmdt_code;
   }
 
   // =============================================================================== //
@@ -20,10 +20,11 @@ class signupController {
   public function isInputEmpty() {
     if (empty($this->username) || empty($this->num_fibra) || empty($this->pwd) || (!isset($this->cmdt_radio)) || $this->cmdt_radio === "sim" && empty($this->cmdt_code)) {
       $result = false;
-    }
-    else {
+    } else {
       $result = true;
     }
     return $result;
   }
 }
+
+// te amo paulo
--- inc/form-handler.inc.php
+++ inc/form-handler.inc.php
@@ -4,6 +4,7 @@
 
   // Verificar se arquivo foi acessado corretamente:
   if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
     // Converter dados em entidades HTML por exemplo:
     // caractere & vira em HTML -> &amp
     $username   = filter_var($_POST["username"],   FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -16,29 +17,20 @@
       require_once "class/dbh.class.php";
       require_once "class/signup-controller.class.php";
 
-      // =============================================================================== //
-      // ======== Se os inputs estiverem vazios alterar estado para exibir erro ======== //
-      if ((new signupController($username, $num_fibra, $pwd, $cmdt_code, $cmdt_radio))->isInputEmpty() === false ) {
+      $stmt = new signupController($username, $num_fibra, $pwd, $cmdt_radio, $cmdt_code);
+
+      if ($stmt->isInputEmpty() === false ) {
         header("Location: ../dist/cadastro.php?submit_state=empty_input");
+        exit();
       } else {
         header("Location: ../dist/cadastro.php?submit_state=success");
+        exit();
       }
     } 
     catch (PDOException $erro) {
       exit("Erro na conexão:<br>".$erro->getMessage());
     }
-
   } else {
     header("Location: ../dist/cadastro.php");
     exit();
   }
-
-/*
-  if (empty($username) || empty($num_fibra) || empty($pwd) || !isset($cmdt_radio) || $cmdt_radio === "sim" && empty($cmdt_code)) {
-    header("Location: ../dist/cadastro.php?submit_state=empty_input");
-    exit();
-  } else {
-    header("Location: ../dist/cadastro.php?submit_state=success");
-  }
-
-  */
